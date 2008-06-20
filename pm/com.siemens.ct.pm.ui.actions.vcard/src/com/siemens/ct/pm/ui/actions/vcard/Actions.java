7a16d8a3245a494cc3a3bdb9a5706fc33b6351fb
toedter_k <toedter_k@bc5761fc-637f-c64d-944f-746135a3b5b4>
2008-06-20 12:25:47 +0000

    Initial import.
    git-svn-id: https://max-server.myftp.org/svn/pm/PM/trunk@71 bc5761fc-637f-c64d-944f-746135a3b5b4

---
+++ pm/com.siemens.ct.pm.ui.actions.vcard/src/com/siemens/ct/pm/ui/actions/vcard/Actions.java
@@ -0,0 +1,60 @@
+package com.siemens.ct.pm.ui.actions.vcard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdesktop.application.Action;
+import org.jdesktop.application.Application;
+import org.jdesktop.application.ApplicationActionMap;
+import org.jdesktop.application.ApplicationContext;
+import org.jdesktop.application.ResourceMap;
+
+import com.siemens.ct.pm.application.service.IActionContribution;
+
+public class Actions implements IActionContribution {
+
+	List<javax.swing.Action> actions;
+
+	public @Action
+	void addVCard() {
+		System.out.println("VCard added...");
+	}
+
+	public @Action
+	void deleteVCard() {
+		System.out.println("VCard deleted...");
+	}
+
+	public @Action
+	void editVCard() {
+		System.out.println("VCard edited...");
+	}
+
+	@Override
+	public List<javax.swing.Action> getActions() {
+		if (actions == null) {
+			actions = new ArrayList<javax.swing.Action>();
+
+			ApplicationContext context = Application.getInstance().getContext();
+			ResourceMap resourceMap = context.getResourceMap(this.getClass());
+			ApplicationActionMap applicationActionMap = new ApplicationActionMap(
+					context, this.getClass(), this, resourceMap);
+			for (Object key : applicationActionMap.allKeys()) {
+				javax.swing.Action action = applicationActionMap.get(key
+						.toString());
+				actions.add(action);
+			}
+		}
+		return actions;
+	}
+
+	@Override
+	public String getMenuLocation() {
+		return "action";
+	}
+
+	@Override
+	public String getToolBarLocation() {
+		return "action";
+	}
+}
