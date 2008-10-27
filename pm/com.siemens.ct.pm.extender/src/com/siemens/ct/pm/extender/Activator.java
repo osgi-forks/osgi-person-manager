0ad2829c86aee33a39b5156990e5bab8e922e347
toedter_k <toedter_k@bc5761fc-637f-c64d-944f-746135a3b5b4>
2008-10-27 14:59:29 +0000

    Heiko's AbstractTracker
    git-svn-id: https://max-server.myftp.org/svn/pm/PM/trunk@100 bc5761fc-637f-c64d-944f-746135a3b5b4

--- pm/com.siemens.ct.pm.extender/src/com/siemens/ct/pm/extender/Activator.java
+++ pm/com.siemens.ct.pm.extender/src/com/siemens/ct/pm/extender/Activator.java
@@ -12,84 +12,19 @@
 
 package com.siemens.ct.pm.extender;
 
-import java.util.HashMap;
-
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleEvent;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.framework.SynchronousBundleListener;
 
-public class Activator implements BundleActivator, SynchronousBundleListener {
+public class Activator implements BundleActivator {
 
-	private BundleContext context;
-	private HashMap<String, ServiceRegistration> serviceMap;
+	private ExtenderBundleTracker extenderBundleTracker;
 
 	public void start(BundleContext context) throws Exception {
-
-		this.context = context;
-		serviceMap = new HashMap<String, ServiceRegistration>();
-
-		// search for existing bundles
-		Bundle[] bundles = context.getBundles();
-		for (Bundle bundle : bundles) {
-			if (Bundle.ACTIVE == bundle.getState()) {
-				addService(bundle);
-			}
-		}
-		context.addBundleListener(this);
+		extenderBundleTracker = new ExtenderBundleTracker(context);
+		extenderBundleTracker.open();
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		context.removeBundleListener(this);
-	}
-
-	@Override
-	public void bundleChanged(BundleEvent event) {
-		if (BundleEvent.STARTED == event.getType()) {
-			addService(event.getBundle());
-		} else if (BundleEvent.STOPPED == event.getType()) {
-			removeService(event.getBundle());
-		}
-	}
-
-	private void removeService(Bundle bundle) {
-		ServiceRegistration serviceRegistration = serviceMap.get(bundle
-				.getSymbolicName());
-		if (serviceRegistration != null) {
-			serviceRegistration.unregister();
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	private void addService(Bundle bundle) {
-		String className = (String) bundle.getHeaders().get(
-				"Action-Contribution");
-		if (className != null) {
-			Class clazz;
-			try {
-				clazz = bundle.loadClass(className);
-				try {
-					ServiceRegistration serviceRegistration = context
-							.registerService(
-									"com.siemens.ct.pm.application.service.IActionContribution",
-									clazz.newInstance(), null);
-					serviceMap.put(bundle.getSymbolicName(),
-							serviceRegistration);
-					System.out
-							.println("Extender: Action Contribution Service registered for: "
-									+ clazz.getName());
-
-				} catch (InstantiationException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				}
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-
+		extenderBundleTracker.close();
 	}
 }
