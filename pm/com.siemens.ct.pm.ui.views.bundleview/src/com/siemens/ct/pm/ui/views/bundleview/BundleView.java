4ea4df47e5ef0ba3d41a6febc411d3fa7c3eb5ff
toedter_k <toedter_k@bc5761fc-637f-c64d-944f-746135a3b5b4>
2009-01-16 14:31:07 +0000

    better table layout
    git-svn-id: https://max-server.myftp.org/svn/pm/PM/trunk@155 bc5761fc-637f-c64d-944f-746135a3b5b4

--- pm/com.siemens.ct.pm.ui.views.bundleview/src/com/siemens/ct/pm/ui/views/bundleview/BundleView.java
+++ pm/com.siemens.ct.pm.ui.views.bundleview/src/com/siemens/ct/pm/ui/views/bundleview/BundleView.java
@@ -35,190 +35,192 @@ import com.siemens.ct.pm.application.service.IViewContribution;
 
 public class BundleView implements IViewContribution {
 
-    private final ImageIcon icon;
-    private final JComponent view;
-    private final JTable table;
-    private final Bundle[] bundles;
+	private final ImageIcon icon;
+	private final JComponent view;
+	private final JTable table;
+	private final Bundle[] bundles;
 
-    @SuppressWarnings("serial")
-    class TableModel extends AbstractTableModel {
+	@SuppressWarnings("serial")
+	class TableModel extends AbstractTableModel {
 
-        @Override
-        public String getColumnName(int column) {
-            switch (column) {
-            case 0:
-                return "";
+		@Override
+		public String getColumnName(int column) {
+			switch (column) {
+			case 0:
+				return "";
 
-            case 1:
-                return "#";
+			case 1:
+				return "#";
 
-            case 2:
-                return "Symbolic Name";
+			case 2:
+				return "Symbolic Name";
 
-            case 3:
-                return "State";
+			case 3:
+				return "State";
 
-            default:
-                return null;
-            }
-        }
+			default:
+				return null;
+			}
+		}
 
-        @Override
-        public int getColumnCount() {
-            return 4;
-        }
+		@Override
+		public int getColumnCount() {
+			return 4;
+		}
 
-        @Override
-        public int getRowCount() {
-            return bundles.length;
-        }
+		@Override
+		public int getRowCount() {
+			return bundles.length;
+		}
 
-        @Override
-        public Object getValueAt(int rowIndex, int columnIndex) {
-            Bundle bundle = bundles[rowIndex];
-            switch (columnIndex) {
-            case 0:
-                return bundle.getState() == Bundle.ACTIVE;
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			Bundle bundle = bundles[rowIndex];
+			switch (columnIndex) {
+			case 0:
+				return bundle.getState() == Bundle.ACTIVE;
 
-            case 1:
-                return bundle.getBundleId();
+			case 1:
+				return bundle.getBundleId();
 
-            case 2:
-                return bundle.getSymbolicName();
+			case 2:
+				return bundle.getSymbolicName();
 
-            case 3:
-                switch (bundle.getState()) {
-                case Bundle.ACTIVE:
-                    return "ACTIVE";
-                case Bundle.INSTALLED:
-                    return "INSTALLED";
-                case Bundle.RESOLVED:
-                    return "RESOLVED";
-                case Bundle.UNINSTALLED:
-                    return "UNINSTALLED";
-                default:
-                    return "UNKNOWN";
-                }
+			case 3:
+				switch (bundle.getState()) {
+				case Bundle.ACTIVE:
+					return "ACTIVE";
+				case Bundle.INSTALLED:
+					return "INSTALLED";
+				case Bundle.RESOLVED:
+					return "RESOLVED";
+				case Bundle.UNINSTALLED:
+					return "UNINSTALLED";
+				default:
+					return "UNKNOWN";
+				}
 
-            default:
-                return null;
-            }
-        }
+			default:
+				return null;
+			}
+		}
 
-        @Override
-        @SuppressWarnings("unchecked")
-        public Class getColumnClass(int c) {
-            return getValueAt(0, c).getClass();
-        }
+		@Override
+		@SuppressWarnings("unchecked")
+		public Class getColumnClass(int c) {
+			return getValueAt(0, c).getClass();
+		}
 
-        @Override
-        public boolean isCellEditable(int row, int col) {
-            if (col == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+		@Override
+		public boolean isCellEditable(int row, int col) {
+			if (col == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 
-        @Override
-        public void setValueAt(Object value, int row, int col) {
-            Bundle selectedBundle = bundles[row];
-            if (selectedBundle.getState() == Bundle.ACTIVE) {
-                try {
-                    selectedBundle.stop();
-                } catch (BundleException e1) {
-                    e1.printStackTrace();
-                }
-            } else if (selectedBundle.getState() == Bundle.RESOLVED) {
-                try {
-                    selectedBundle.start();
-                } catch (BundleException e1) {
-                    e1.printStackTrace();
-                }
-            }
-            fireTableDataChanged();
-        }
-    }
+		@Override
+		public void setValueAt(Object value, int row, int col) {
+			Bundle selectedBundle = bundles[row];
+			if (selectedBundle.getState() == Bundle.ACTIVE) {
+				try {
+					selectedBundle.stop();
+				} catch (BundleException e1) {
+					e1.printStackTrace();
+				}
+			} else if (selectedBundle.getState() == Bundle.RESOLVED) {
+				try {
+					selectedBundle.start();
+				} catch (BundleException e1) {
+					e1.printStackTrace();
+				}
+			}
+			fireTableDataChanged();
+		}
+	}
 
-    public BundleView() {
-        super();
+	public BundleView() {
+		super();
 
-        bundles = Activator.getBundles();
+		bundles = Activator.getBundles();
 
-        icon = new ImageIcon(this.getClass().getResource("/icons/bundle.png"));
-        table = new JTable(new TableModel());
-        table.setColumnSelectionAllowed(false);
-        table.setRowSelectionAllowed(true);
+		icon = new ImageIcon(this.getClass().getResource("/icons/bundle.png"));
+		table = new JTable(new TableModel());
+		table.setColumnSelectionAllowed(false);
+		table.setRowSelectionAllowed(true);
 
-        table.getColumnModel().getColumn(0).setPreferredWidth(25);
-        table.getColumnModel().getColumn(0).setMaxWidth(25);
-        table.getColumnModel().getColumn(1).setPreferredWidth(25);
-        table.getColumnModel().getColumn(1).setMaxWidth(25);
-        table.getColumnModel().getColumn(2).setPreferredWidth(200);
+		table.getColumnModel().getColumn(0).setPreferredWidth(25);
+		table.getColumnModel().getColumn(0).setMaxWidth(25);
+		table.getColumnModel().getColumn(1).setPreferredWidth(30);
+		table.getColumnModel().getColumn(1).setMaxWidth(40);
+		table.getColumnModel().getColumn(2).setPreferredWidth(200);
 
-        table.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.getSelectionModel().setSelectionMode(
+				ListSelectionModel.SINGLE_SELECTION);
 
-        view = new JPanel();
-        view.setLayout(new BorderLayout());
+		view = new JPanel();
+		view.setLayout(new BorderLayout());
 
-        JPanel selectionPanel = new JPanel();
-        JButton selectAll = new JButton("Select All");
-        selectAll.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                for (Bundle bundle : bundles) {
-                    try {
-                        bundle.start();
-                    } catch (BundleException e1) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace();
-                    }
-                }
-                table.revalidate();
-            }
-        });
-        JButton deselectAll = new JButton("Deselect All");
-        deselectAll.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                for (Bundle bundle : bundles) {
-                    try {
-                        bundle.stop();
-                    } catch (BundleException e1) {
-                        // TODO Auto-generated catch block
-                        e1.printStackTrace();
-                    }
-                }
-                table.revalidate();
-            }
-        });
-        selectionPanel.add(selectAll);
-        selectionPanel.add(deselectAll);
-        view.add(selectionPanel, BorderLayout.SOUTH);
+		JPanel selectionPanel = new JPanel();
+		JButton selectAll = new JButton("Select All");
+		selectAll.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (Bundle bundle : bundles) {
+					try {
+						bundle.start();
+					} catch (BundleException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				table.revalidate();
+			}
+		});
+		JButton deselectAll = new JButton("Deselect All");
+		deselectAll.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (Bundle bundle : bundles) {
+					try {
+						bundle.stop();
+					} catch (BundleException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				table.revalidate();
+			}
+		});
+		selectionPanel.add(selectAll);
+		selectionPanel.add(deselectAll);
+		view.add(selectionPanel, BorderLayout.SOUTH);
 
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(2,
-                2, 2, 2), BorderFactory.createLineBorder(Color.lightGray)));
-        view.add(scrollPane, BorderLayout.CENTER);
-    }
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBorder(BorderFactory.createCompoundBorder(BorderFactory
+				.createEmptyBorder(2, 2, 2, 2), BorderFactory
+				.createLineBorder(Color.lightGray)));
+		view.add(scrollPane, BorderLayout.CENTER);
+	}
 
-    @Override
-    public Icon getIcon() {
-        return icon;
-    }
+	@Override
+	public Icon getIcon() {
+		return icon;
+	}
 
-    @Override
-    public String getName() {
-        return "Bundle View";
-    }
+	@Override
+	public String getName() {
+		return "Bundle View";
+	}
 
-    @Override
-    public JComponent getView() {
-        return view;
-    }
+	@Override
+	public JComponent getView() {
+		return view;
+	}
 
-    @Override
-    public int getPosition() {
-        return 100;
-    }
+	@Override
+	public int getPosition() {
+		return 100;
+	}
 }
